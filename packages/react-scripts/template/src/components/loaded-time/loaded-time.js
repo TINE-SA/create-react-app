e68d023dfeffc7e3f535e2c8353ad755829955ff
Tom Erik Støwer <testower@gmail.com>
2018-08-29 21:32:06 +0200

    Fix proptype validation error

--- packages/react-scripts/template/src/components/loaded-time/loaded-time.js
+++ packages/react-scripts/template/src/components/loaded-time/loaded-time.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import StyledLoadedTime from './styled-loaded-time';
 import lang from 'constants/lang';
+
 class LoadedTime extends Component {
   render() {
     const { loadedTime } = this.props;
-
     return <StyledLoadedTime>{lang.LOADED_AT`${loadedTime}`}</StyledLoadedTime>;
   }
 }
+
+LoadedTime.propTypes = {
+  loadedTime: PropTypes.string.isRequired
+};
 
 export default LoadedTime;
