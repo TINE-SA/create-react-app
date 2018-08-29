1829267ba4b913039c0b4341472754fbfc921a38
Tom Erik Støwer <testower@gmail.com>
2018-08-29 20:07:01 +0200

    Use injectGlobal from styled instead of a css file

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from 'containers/app';
+import { injectGlobal } from 'styled-components';
+
 // If we need to use serviceWorker, we could tap into built-in functionality.
 // import registerServiceWorker from './registerServiceWorker';
+
+import App from 'containers/app';
+
+// This is the only place in the project where global styles
+// are allowed. Use it for resets etc but keep it to a
+// minimum.
+// https://www.styled-components.com/docs/api#injectglobal
+injectGlobal(`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
+`);
 
 ReactDOM.render(<App />, document.getElementById('app'));
 // registerServiceWorker();
